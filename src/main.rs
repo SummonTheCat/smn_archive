b745797b8b897022217f4c7fd989830f1427226e
Julien Liakos <julien_andrew@outlook.com>
2024-10-03 21:12:37 +0200

    Cleaning comments

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod core;
 mod tooling;
 
 fn main() {
-    // Run the cmd tools for testing and utility
-    // E.g. cargo run -- tool arg1 arg2
+    /*
+    Run the cmd tools for testing and utility
+    E.g. cargo run -- tool arg1 arg2
+    */
     tooling::cli::run_cmd();
 }   
